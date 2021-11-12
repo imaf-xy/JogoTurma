242101303b3378a165607533f3ebc094eb3620a0
imaf-xy <83644647+imaf-xy@users.noreply.github.com>
2021-11-12 14:40:58 -0300

    Add files via upload

---
+++ Pontuacao.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Pontuacao : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   public void OnMouseDown()
+    {
+        float pontuacao = transform.position.y - GameObject.Find("Barra").transform.position.y ;
+        
+
+        Debug.Log(Math.Round(100 - (pontuacao * 10),0));
+
+
+    }
+
+
+}
